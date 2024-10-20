b7898f8e4f538e7d927ab158cc3b98e45442c038
juan <451586.joan23@fje.edu>
2024-10-20 11:02:01 +0200

    testear circulo

---
+++ classes/Circulo.php
@@ -0,0 +1,20 @@
+<?php
+    require_once './Circulo.php';
+    class Circulo extends FiguraGeometrica{
+        public function __construct($tipoFigura,$lado1){
+            parent::__construct($tipoFigura,$lado1);
+        }
+        public function calcularArea(){
+            return pi() * pow($this->lado1, 2);
+        }
+        public function calcularPerimetro(){
+            return 2 * pi() * $this->lado1;
+        }
+        public function __tostring(){
+            return "<p><strong>Figura: </strong>" . $this->tipoFigura . "</p>" .
+            "<p><strong>Radio: </strong>". $this->lado1. "</p>".
+            "<h3><strong>RESULTADOS</strong></h3>" .
+            "<p><strong>Área: </strong>". $this->calcularArea(). "</p>".
+            "<p><strong>Perímetro: </strong>". $this->calcularPerimetro(). "</p>";
+        }
+    }
