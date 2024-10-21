b33b73b63157af141a8900e658b736c8ac7229e3
juan <451586.joan23@fje.edu>
2024-10-21 17:04:18 +0200

    finish

--- classes/Triangulo.php
+++ classes/Triangulo.php
@@ -8,5 +8,20 @@
             $this->lado2 = $lado2;
             $this->lado3 = $lado3;
         }
-        
+        public function calcularArea(){
+            $s = ($this->lado1 + $this->lado2 + $this->lado3) / 2;
+            return sqrt($s * ($s - $this->lado1) * ($s - $this->lado2) * ($s - $this->lado3));
+        }
+        public function calcularPerimetro(){
+            return $this->lado1 + $this->lado2 + $this->lado3;
+        }
+        public function __tostring(){
+            return "<p><strong>Figura: </strong>" . $this->tipoFigura . "</p>" . 
+            "<p><strong>Cateto A: </strong> " . $this->lado1 . " </p>" . 
+            "<p><strong>Cateto B: </strong> " . $this->lado2 . " </p>" . 
+            "<p><strong>Cateto C: </strong> " . $this->lado3 . " </p>" . 
+            "<h3><strong>RESULTADOS</strong></h3>" .
+            "<p><strong>Area: </strong> ". $this->calcularArea(). "</p>" . 
+            "<p><strong>Perimetro: </strong> " . $this->calcularPerimetro() . "</p>";
+        }
     }
